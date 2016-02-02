Added residuals to barywidth and fixed SNR calculation

lib.py
@@ -18,6 +18,7 @@ from scipy.optimize import curve_fit
 
 
 
+FLOAT_DTYPE = 'float64'
 CPLX_DTYPE = 'complex128'
 INT_DTYPE = 'int64'
 
@@ -71,11 +72,12 @@ def cplx_gaussian(shape, noise_variance):
 #------------
 def calc_snr(ctrl,p):
     """Calculates the SNR of the system provided"""
-    noise_variance = np.float64(ctrl.noise_std**2)
-    signal_variance = np.float64(np.var(p.analog_sig))
+    noise_variance = np.float64(ctrl.noise_std)**2
+    signal_power = np.float64((np.sum(np.abs(p.analog_sig))+1)/len(p.analog_sig))**2
+
 
     if not noise_variance == 0:
-        snr = signal_variance/noise_variance
+        snr = signal_power/noise_variance
     else:
         snr = np.float64('inf')
 
@@ -379,15 +381,17 @@ def build_timestamp_id():
 
 
 #------------------------
-def barywidth_map(p, reach=0.05, scaling=0.001, force_calculate=False, disp=False):
+def barywidth_map(p, reach=0.05, scaling_fct=100, force_calculate=False, disp=False):
     """Generates the barywidth map for a given range, given as a fraction of f_symb
     If the map already exists, it pulls it from the sql database instead"""
 
     """It also does a linear regression to the data"""
 
 
-    if not (reach/scaling).is_integer():
-        warnings.warn("The ratio reach/scaling must be an integer")
+    if not float(scaling_fct).is_integer():
+        ValueError('Scaling must be an integer')
+
+    scaling = reach/scaling_fct
 
 
     dbase_file = 'barywidths.sqlite'
@@ -721,8 +725,8 @@ class Params(Struct):
 
         # A chain of ZC
         if self.train_type == 'chain':
-            zeros_count = round(self.zc_len*self.central_padding) + 1
-            if zeros_count % 2 == 0: zeros_count -= 1
+            zeros_count = round(self.zc_len*self.central_padding) +1
+            if zeros_count % 2 == 0 and zeros_count > 2: zeros_count -= 1
 
             training_seq = np.concatenate(tuple([zneg]*self.repeat+[np.zeros(zeros_count)]+[zpos]*self.repeat))
 

plotlib.py
@@ -51,9 +51,10 @@ def discrete(*args, repad_ratio=0.1):
     fh = plt.figure()
 
     plt.plot((x, x) , (y, np.zeros(len(y))), 'k-')
-    #plt.scatter(x, y, marker='.')
-    #plt.plot(indep_ax,curve,'k-')
-    x_lims = [min(x), max(x)]
+
+    # Pad the limits so we always see the leftmost-rightmost point
+    x_pad = (x[1]-x[0])
+    x_lims = [min(x)-x_pad, max(x)+x_pad]
     plt.plot(x_lims, [0,0], 'k-')
     plt.xlim(x_lims)
     return fh
@@ -122,66 +123,78 @@ def hair(samples,param, y_label='Parameter', savename=''):
 
 
 #---------------
-def barywidth(*args, savename='', fit_type='linear', **kwargs):
+def barywidth(*args, savename='', fit_type='linear', residuals=False, **kwargs):
     """Accepts either a Params() object or two iterables representing the CFO and the barywidth"""
 
 
     if len(args) == 1 and type(args[0]).__name__ == 'Params':
         CFO, barywidths = barywidth_map(args[0], **kwargs)
-        CFO = CFO
+        f_symb = args[0].f_symb
     elif len(args) == 2:
         CFO = args[0]
         barywidths = args[1]
+        f_symb = 1
+        fit_type = 'none'
     else:
         print("Invalid input")
 
-
+    x = CFO/f_symb
+    y = barywidths
     
-    fit = np.empty(2)
-    fit[0] = args[0].baryslope*args[0].f_symb
-    fit[1] = args[0].basewidth
+    fh = plt.figure()
+    main_axes_loc = [.13,.3,.8,.6] 
+    frame1 = fh.add_axes(main_axes_loc)
 
-    f_symb = args[0].f_symb
+    lh = plt.plot(x, y, 'k-', label='Barycenter width')
+    x_lims = [min(x), max(x)]
+    plt.plot((0,0), (min(y),max(y)))
 
-    
-    continuous(CFO/args[0].f_symb,barywidths)
-    plt.plot((0,0), (min(barywidths),max(barywidths)))
-
-    # Linear fit display
+    fit_curve = None
     if fit_type == 'linear':
-        fitleft = fit[0]*CFO[0]/f_symb + fit[1]
-        fitright= fit[0]*CFO[-1]/f_symb + fit[1]
-        plt.plot((CFO[0]/args[0].f_symb, CFO[-1]/args[0].f_symb), (fitleft, fitright))
+    # Linear fit display
+        fit = np.empty(2)
+        fit[0] = args[0].baryslope*f_symb
+        fit[1] = args[0].basewidth
+        fit_curve = fit[0]*x + fit[1]
+        plt.plot(x, fit_curve)
     elif fit_type == 'order2':
         # parabola fit display
         fit = args[0].order2fit
         fit_curve = fit[0]*CFO**2 + fit[1]*CFO**1 + args[0].basewidth#fit[2]
-        plt.plot(CFO/args[0].f_symb, fit_curve)
-    elif fit_type == 'step_sin':
-        pass
+        plt.plot(x, fit_curve)
     elif fit_type == 'none':
         pass
     else:
         print('Unknown fit option. No fit displayed')
     
 
-    plt.xlabel('CFO (f_symb)')
+    xlabel = 'CFO (f_symb)'
+    plt.xlabel(xlabel)
     plt.ylabel('Barycenter width')
+    plt.xlim(x_lims)
+
+    # Plotting residuals
+    if fit_curve is not None and residuals:
+        residuals_vals = fit_curve - barywidths
+        residuals_curve = residuals_vals/np.std(residuals_vals)
+        frame1.set_xticklabels([]) 
+
+        residuals_loc = [main_axes_loc[0],.1,.8,0]
+        residuals_loc[3] = main_axes_loc[1]-residuals_loc[1]-0.005
+        frame2 = fh.add_axes(residuals_loc) 
+        plt.plot((x[0],x[-1]),(0,0), 'k-')
+        plt.plot(x,residuals_curve)
+        frame2.yaxis.set_ticks((-1,1))
+        plt.ylabel('\sigma')
+        plt.xlabel(xlabel)
+        plt.xlim(x_lims)
     
 
     save(savename)
 
 
-#----------------
+#----------------------------
 def crosscorr(p, savename='', is_zpos=True):
-    """Builds a crosscorrelation graph from the crosscorrelation with zpos (default)
-    Accepted input:
-    (<class 'Params'>)      will plot the crosscorrelation with the analog signal 
-    """
-    tmp = p.full_sim # Save temporary fullsim value
-    p.full_sim = False
-
-    _, _, rpos, rneg = calc_both_barycenters(p,mode='same')
     p.full_sim = tmp # Restore entrance value
 
     y = rpos if is_zpos else rneg
@@ -274,12 +287,12 @@ def pulse(p,savename=''):
     
     y = np.real(p.pulse)
     x = np.arange(len(y)) - len(y)/2 + 0.5
-    x = x/p.f_samp
+
 
     discrete(x,y)
 
-    plt.xlabel('t')
-    plt.ylabel('p(t)')
+    plt.xlabel('n (samples)')
+    plt.ylabel('p[n])')
     
     save(savename)
     return x,y

sim_channel.py
@@ -20,11 +20,13 @@ class SimControls(lib.Struct):
     """Container object for the control parameters of the runsim() function"""
     def __init__(self):
         """Default values"""
+        self.steps = 30
         self.nodecount = 7
         self.basephi = 1000
         self.chansize = self.basephi*50
         self.noise_std = 0
         self.phi_bounds = [1,1]
+        self.theta_bounds = [0,1]
         self.self_emit = False # IF set to False, the self-emit will just be skipped.
         self.CFO_step_wait = 60
         self.rand_init = False
@@ -35,6 +37,9 @@ class SimControls(lib.Struct):
         self.cfo_bias = 0 # in terms of f_samp
         self.delay_fct = lib.delay_pdf_static
         self.deltaf_bound = 0.02 # in units of f_samp
+        self.bmap_reach = 3e-1
+        self.bmap_scaling = 100
+        self.non_rand_seed = 1231231
         # Echo controls
         self.max_echo_taps = 4
         self.min_delay = 0
@@ -44,6 +49,15 @@ class SimControls(lib.Struct):
         self.max_CFO_correction = 0.02 # As a factor of f_symb
         self.CFO_processing_avgtype = 'mov_avg' # 'mov_avg' or 'reg' (non-mov avg)
         self.CFO_processing_avgwindow = 5
+
+        self.init_update = True
+
+    def update(self):
+        """Must be run before runsim can be executed"""
+        self.chansize = int(self.basephi*self.steps)
+        self.phi_minmax = [round(x*self.basephi) for x in self.phi_bounds]
+        self.theta_minmax = [round(x*self.basephi) for x in self.theta_bounds]
+        self.init_update = True
 
 #-------------------------
 def ordered_insert(sample, clknum):
@@ -109,7 +123,6 @@ def runsim(p,ctrl):
     basephi = ctrl.basephi
     chansize = ctrl.chansize
     noise_std = ctrl.noise_std
-    phi_bounds = ctrl.phi_bounds
     self_emit = ctrl.self_emit
     CFO_step_wait = ctrl.CFO_step_wait
     epsilon_TO = ctrl.epsilon_TO
@@ -117,9 +130,10 @@ def runsim(p,ctrl):
     cfo_mapper_fct = ctrl.cfo_mapper_fct
     CFO_processing_avgtype = ctrl.CFO_processing_avgtype
     CFO_processing_avgwindow = ctrl.CFO_processing_avgwindow
+    phi_minmax = ctrl.phi_minmax
+    theta_minmax = ctrl.theta_minmax
 
     # IF echoes specified, to shove in array. OW, just don't worry about it
-    do_echoes = True
     try:
         echo_delay = ctrl.echo_delay
         echo_amp = ctrl.echo_amp
@@ -137,6 +151,8 @@ def runsim(p,ctrl):
     # INPUT EXCEPTIONS
     if not p.init_update or not p.init_basewidth:
         raise AttributeError("Need to run p.update() and p.calc_base_barywidth before calling runsim()")
+    if not ctrl.init_update:
+        raise AttributeError("Need to run ctrl.update() before calling runsim()")
     if len(analog_pulse) > basephi:
         raise ValueError('Pulse is longer than a sample. Bad stuff will happen')
 
@@ -151,7 +167,6 @@ def runsim(p,ctrl):
     queue_clk = []
     pulse_len = len(analog_pulse)
     offset = int((pulse_len-1)/2)
-    channels = lib.cplx_gaussian( [nodecount,chansize],noise_std) 
     max_sample = chansize-offset-np.max(echo_delay);
     wait_til_adjust = np.zeros(nodecount, dtype='int64')
     wait_til_emit = np.zeros(nodecount, dtype='int64')
@@ -166,8 +181,7 @@ def runsim(p,ctrl):
 
 
     
-    # Clock initial values
-    phi_minmax = [round(x*basephi) for x in phi_bounds]
+    # Node initial values
     deltaf_minmax = np.array([-1*ctrl.deltaf_bound,ctrl.deltaf_bound])*p.f_symb
     do_CFO_correction = np.array([False]*nodecount)
     wait_CFO_correction = np.zeros(nodecount)
@@ -175,25 +189,19 @@ def runsim(p,ctrl):
     CFO_corr_list = [[] for x in range(nodecount)]
     TO_corr_list = [[] for x in range(nodecount)]
 
-    if ctrl.rand_init:
-        phi = np.random.randint(phi_minmax[0],phi_minmax[1]+1, size=nodecount)
-        theta = np.random.randint(basephi, size=nodecount)
-        #theta = np.zeros(nodecount).astype(int) + int(round(basephi/2))
-        deltaf = np.random.uniform(deltaf_minmax[0],deltaf_minmax[1], size=nodecount)
-        clk_creation = np.random.randint(0,chansize, size=nodecount)
-
-    else:
-        phi = np.random.randint(phi_minmax[0],phi_minmax[1]+1, size=nodecount)
-        theta = np.round((np.arange(nodecount)/(2*(nodecount-1))+0.25) * basephi)
-        #theta = np.zeros(nodecount) + round(basephi/2)
-        theta = theta.astype(int)
-        #theta = np.zeros(nodecount).astype(int)
-        #deltaf = ((np.arange(nodecount)**2 - nodecount/2)/nodecount**2) * deltaf_minmax[1]
-        deltaf = (np.arange(nodecount)-nodecount/2)/nodecount * deltaf_minmax[1]
-        #deltaf = np.zeros(nodecount)
-        clk_creation = np.zeros(nodecount)
-
+    if not ctrl.rand_init:
+        np.random.seed(ctrl.non_rand_seed)
     
+    phi = np.random.randint(phi_minmax[0],phi_minmax[1]+1, size=nodecount)
+    theta = np.random.randint(theta_minmax[0],theta_minmax[1]+1, size=nodecount)
+    deltaf = np.random.uniform(deltaf_minmax[0],deltaf_minmax[1], size=nodecount)
+    clk_creation = np.random.randint(0,chansize, size=nodecount)
+    channels = lib.cplx_gaussian( [nodecount,chansize],noise_std) 
+
+    if not ctrl.rand_init:
+        np.random.seed()
+
+
 
     # First events happen on initial phase shift
     for clknum, sample in enumerate(theta):